25c748da97dca6e9cf142f91bdb2d17d7cd8d40f
WatcherSky <theworld.com@mail.ru>
2021-10-13 12:12:41 +0600

    Commit Unit Testing

---
+++ WorkWithApi/Evaluation Test AppTests/Evaluation_Test_AppTests.swift
@@ -0,0 +1,30 @@
+//
+//  Evaluation_Test_AppTests.swift
+//  Evaluation Test AppTests
+//
+//  Created by Владимир on 13.10.2021.
+//
+
+import XCTest
+@testable import Evaluation_Test_App
+
+class Evaluation_Test_AppTests: XCTestCase {
+   private  var resultsData: ResultsData?
+   private  var networkManager = NetworkManager()
+   private  let urlStringRequest = "https://itunes.apple.com/search?term=Happier&media=music&entity=album"
+    
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+        resultsData = nil
+    }
+
+    func testForJsonGetData() throws { //Test if data from json isnt Nil
+        networkManager.getAlbums(urlString: urlStringRequest) { (resultsData) in
+            guard let resultsData = resultsData else { return }
+            self.resultsData = resultsData
+            XCTAssertNotNil(resultsData)
+        }
+    }
+}
